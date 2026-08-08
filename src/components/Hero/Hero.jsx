b8c16c346541cab93618f908ccd75cd16e075a44
Daniel Jilek <jilek.d@northeastern.edu>
2026-08-07 21:32:05 -0500

    Added headshot

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,20 +2,31 @@ import { site } from '../../data/site'
 import SocialIcons from '../SocialIcons/SocialIcons'
 import './Hero.css'
 
+const headshotSrc = `${import.meta.env.BASE_URL}headshot.jpg`
+
 export default function Hero() {
   return (
     <section className="hero" id="top" aria-labelledby="hero-heading">
-      <p className="hero__eyebrow">Hi, I&apos;m</p>
-      <h1 className="hero__title" id="hero-heading">
-        {site.name}
-      </h1>
-      <p className="hero__role">{site.role}</p>
-      <p className="hero__tagline">{site.tagline}</p>
-      <SocialIcons className="hero__social" />
-      <div className="hero__actions">
-        <a className="hero__btn hero__btn--primary" href="#projects">
-          View projects
-        </a>
+      <div className="hero__inner">
+        <img
+          className="hero__headshot"
+          src={headshotSrc}
+          alt={`${site.name} headshot`}
+          width={240}
+          height={240}
+        />
+        <p className="hero__eyebrow">Hi, I&apos;m</p>
+        <h1 className="hero__title" id="hero-heading">
+          {site.name}
+        </h1>
+        <p className="hero__role">{site.role}</p>
+        <p className="hero__tagline">{site.tagline}</p>
+        <SocialIcons className="hero__social" />
+        <div className="hero__actions">
+          <a className="hero__btn hero__btn--primary" href="#projects">
+            View projects
+          </a>
+        </div>
       </div>
     </section>
   )
